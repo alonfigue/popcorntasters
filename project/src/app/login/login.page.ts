728d85eae5c7612906ff319f4daf0a33270212fe
alonfigue <alonsogfm2000@gmail.com>
2022-03-11 21:06:16 -0400

    Login and register process pending

--- project/src/app/login/login.page.ts
+++ project/src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,58 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
-  constructor() { }
-
+  
+  constructor(private router: Router) { }
+  
   ngOnInit() {
   }
-
+  
+  username:string;
+  password:string;
+  
+  login(){
+    
+    const username = this.username;
+    const password = this.password;  
+    
+    console.log(username, password);
+    
+    fetch('http://localhost:3000/auth/login',{
+    method: 'POST',
+    headers: new Headers({
+      // Encabezados
+      'Content-Type': 'application/json'
+    }),
+    body: JSON.stringify(
+      {    
+        "username": username,
+        "password": password
+      })
+      
+    }).then(response=>{
+      console.log(response);
+      this.router.navigate(['/main']);
+      return response.json()
+    }).then(r =>{
+      console.log(r);
+    }).catch(e => console.log(e))
+    
+  }
 }
+
+/*import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+  
+  constructor() { }
+  
+  ngOnInit() {
+  }
+  
+}
+*/
--- project/src/app/register/register.page.ts
+++ project/src/app/register/register.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+/*import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +6,70 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
-
+  
   constructor() { }
-
+  
   ngOnInit() {
   }
+  
+}*/
 
+import { Component, OnInit } from '@angular/core';
+import { Router} from '@angular/router';
+
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.page.html',
+  styleUrls: ['./register.page.scss'],
+})
+export class RegisterPage implements OnInit {
+  
+  constructor(private router: Router) { }
+  
+  ngOnInit() {
+  }
+  
+  name:string;
+  username:string;
+  pass:string;
+  cpass:string;
+  
+  register(){
+    const name = this.name;
+    const username = this.username;
+    const pass = this.pass;
+    const cpass = this.cpass;
+    
+    fetch('http://localhost:3000/auth/register',{
+    method: 'POST',
+    headers: new Headers({
+      // Encabezados
+      'Content-Type': 'application/json'
+    }),
+    body: JSON.stringify(
+      {
+        "name": name,
+        "username": username,
+        "pass": pass,
+        "cpass": cpass
+      })
+      
+    }).then(response=>{
+      console.log(response);
+      if (response.redirected == true)
+      {
+        window.location.replace(response.url)
+      }
+      return response.json()
+    }).then(r =>{
+      console.log(r);
+    }).catch(e => console.log(e))
+    
+    //var p = document.createElement("p");
+    //cont1.appendChild(p);
+    //p.innerHTML = "Succesfully updated";
+    
+    this.router.navigate(['/login']);
+  }
 }
