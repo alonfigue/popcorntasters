9bfb525e9831ff9c55be26fb7a0c4c6536f20fb8
alonfigue <alonsogfm2000@gmail.com>
2022-03-13 13:19:39 -0400

    Log in view v1

--- frontend/src/app/pages/login/login.module.ts
+++ frontend/src/app/pages/login/login.module.ts
@@ -7,12 +7,14 @@ import { IonicModule } from '@ionic/angular';
 import { LoginPageRoutingModule } from './login-routing.module';
 
 import { LoginPage } from './login.page';
+import { DataInputModule } from 'src/app/components/data-input/data-input.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    DataInputModule,
     LoginPageRoutingModule
   ],
   declarations: [LoginPage]
