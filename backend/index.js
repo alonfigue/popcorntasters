301215cf04d39811a198b1ed97a4cab6d171e026
Alonso Figueroa <65868683+alonfigue@users.noreply.github.com>
2022-03-12 22:59:15 -0400

    Requiring and using the cors

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,7 @@ const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
 const fs = require('fs');
-const cors = require('cors');
-
-
+const cors= require('cors');
 const app = express();
 const mongoose = require('mongoose');
 const User = require('./models/user');
@@ -12,10 +10,12 @@ const User = require('./models/user');
 
 mongoose.set('useFindAndModify', false);
 
-app.use(cors());
+
 app.use(express.json());
 app.use(morgan('dev'));
-
+app.use(cors({
+    origin: '*'
+}));
 
 app.get('/',  async (req, res)=>{
 
@@ -25,9 +25,10 @@ app.get('/',  async (req, res)=>{
 //routes
 const user_router = require('./routes/users');
 const auth_router = require('./routes/auth');
+const movie_router = require('./routes/movies');
 
 app.use('/users', user_router);
 app.use('/auth', auth_router);
-
+app.use('/movies', movie_router);
 
 module.exports = app
