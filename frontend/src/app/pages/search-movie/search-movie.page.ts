ae9f66332c902e15f52cebdc08e8ec0da0a242e9
alonfigue <alonsogfm2000@gmail.com>
2022-03-16 06:59:04 -0400

    Adding more movies when searching for a movie that is not in the array that contains the movies that are shown in the search movie page

--- frontend/src/app/pages/search-movie/search-movie.page.ts
+++ frontend/src/app/pages/search-movie/search-movie.page.ts
@@ -7,21 +7,37 @@ import { HttpClient } from "@angular/common/http";
   styleUrls: ['./search-movie.page.scss'],
 })
 export class SearchMoviePage implements OnInit {
-
+  
   searchTerm: string;
   movieS = [];
-
+  moreMovies= [];
+  
   constructor(private http: HttpClient) { }
-
+  
   ngOnInit() {
-
+    
     this.http.get<any>('http://localhost:3000/movies/')
     .subscribe((res) => {
       console.log(res);
       this.movieS = res;
       console.log(this.movieS);
     });
-
+    
   }
+  
+  addMovies(){
 
+    const searchTerm = this.searchTerm;
+
+    console.log('this is working')
+    
+    this.http.get<any>('http://localhost:3000/movies/' + this.searchTerm)
+    .subscribe((res) => {
+      console.log(res);
+      this.moreMovies = res;
+      console.log(this.moreMovies);
+    });
+    
+  }
+  
 }
