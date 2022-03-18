8c0c474e25d2f723f993dae89b68a85d16c75468
Ricardo Muchacho <ricardojrmuchacho2@gmail.com>
2022-03-18 13:11:10 -0400

    added apk

--- frontend/src/app/pages/profile-user-delete/profile-user-delete.page.ts
+++ frontend/src/app/pages/profile-user-delete/profile-user-delete.page.ts
@@ -9,43 +9,42 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./profile-user-delete.page.scss'],
 })
 export class ProfileUserDeletePage implements OnInit {
-  
+  username = localStorage.getItem('username');
+
   constructor(
     private router: Router,
     private http: HttpClient,
     private activatedRoute: ActivatedRoute
-    ) {}
-    
-    ngOnInit() {}
-    
-    username: string;
-    password: string;
-    
-    deleteUser() {
-      const username = this.username;
-      const password = this.password;
-      
-      fetch('https://popcorntasters-api.herokuapp.com/users/' + this.username, {
+  ) {}
+
+  ngOnInit() {}
+
+  password: string;
+
+  deleteUser() {
+    const password = this.password;
+
+    fetch('https://popcorntasters-api.herokuapp.com/users/' + this.username, {
       method: 'DELETE',
       headers: new Headers({
         // Encabezados
         'Content-Type': 'application/json',
       }),
       body: JSON.stringify({
-        username: username,
+        username: this.username,
         password: password,
       }),
     })
-    .then((response) => {
-      console.log(response);
-      console.log('Account eliminated...');
-      localStorage.removeItem('loggedin');
-      this.router.navigate(['/login']);
-      return response.json();
-    })
-    .then((r) => {
-      console.log(r);
-    })
-    .catch((e) => console.log(e));
+      .then((response) => {
+        console.log(response);
+        console.log('Account eliminated...');
+        localStorage.removeItem('loggedin');
+        this.router.navigate(['/login']);
+        return response.json();
+      })
+      .then((r) => {
+        console.log(r);
+      })
+      .catch((e) => console.log(e));
   }
 }
