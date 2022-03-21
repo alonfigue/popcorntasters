fa8a5f0561350a36b9c80cc4459270c09114bd99
alonfigue <alonsogfm2000@gmail.com>
2022-03-21 15:09:37 -0400

    Making the app more intuitive

--- frontend/src/app/pages/register/register.page.ts
+++ frontend/src/app/pages/register/register.page.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { AlertController, NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-register',
@@ -8,106 +7,44 @@ import { AlertController, NavController } from '@ionic/angular';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
-  constructor(private router: Router, public alertController: AlertController, 
-    public navCtrl: NavController,) {}
+  constructor(private router: Router) {}
+  
+  ngOnInit() {}
+  
+  username: string;
+  name: string;
+  password: string;
+  
+  register() {
+    const username = this.username;
+    const name = this.name;
+    const password = this.password;
     
-    ngOnInit() {}
-    
-    username: string;
-    name: string;
-    password: string;
-    
-    async register() {
-      
-      
-      const username = this.username;
-      const name = this.name;
-      const password = this.password;
-      
-      
-      
-      
-      const alert = await this.alertController.create({
-        header: 'Registration',
-        message: 'Successfully done',
-        buttons: [
-          {
-            text: 'OK',
-            handler: () => { 
-              
-              
-              
-              fetch('https://popcorntasters-api.herokuapp.com/auth/register', {
-              method: 'POST',
-              headers: new Headers({
-                // Encabezados
-                'Content-Type': 'application/json',
-              }),
-              body: JSON.stringify({
-                username: username,
-                name: name,
-                password: password,
-              }),
-            })
-            .then((response) => {
-              console.log(response);
-              if (response.redirected == true) {
-                window.location.replace(response.url);
-              }
-              
-              console.log('New account created...');
-              this.router.navigate(['/login']);
-              return response.json();
-            })
-            .then((r) => {
-              console.log(r);
-            })
-            .catch((e) => console.log(e));
-            
-            
-          }
-          
-          
-          
-          
-        }
-      ]
-    });    
-    
-    
-    await alert.present();
-    
-  }
+    fetch('https://popcorntasters-api.herokuapp.com/auth/register', {
+    method: 'POST',
+    headers: new Headers({
+      // Encabezados
+      'Content-Type': 'application/json',
+    }),
+    body: JSON.stringify({
+      username: username,
+      name: name,
+      password: password,
+    }),
+  })
+  .then((response) => {
+    console.log(response);
+    if (response.redirected == true) {
+      window.location.replace(response.url);
+    }
+    console.log('New account created...');
+    this.router.navigate(['/login']);
+    return response.json();
+  })
+  .then((r) => {
+    console.log(r);
+  })
+  .catch((e) => console.log(e));
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
